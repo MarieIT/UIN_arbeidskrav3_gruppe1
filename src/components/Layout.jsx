25e9e69b71ac3c307dad66396519403b577e294d
Lubbsaur <berghjunior@gmail.com>
2025-04-10 00:01:54 +0200

    Pushet med trusty, ol' Github Desktop!

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,19 @@
-export default function Layout() {
-  return <h1>Morra di</h1>
+import { Link } from "react-router-dom";
+
+export default function Layout({ children }) {
+  return (
+    <>
+    <header>
+      <nav>
+        <ul>
+          <li><Link to="members/">Member</Link></li>
+          <li><Link to="/">Home</Link></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      {children}
+    </main>
+    </>
+  )
 }
