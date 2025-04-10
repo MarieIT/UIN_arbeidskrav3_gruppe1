c064d06eff8df8e713c7c2d9fb54a460e4477abb
MarieIT <142742570+MarieIT@users.noreply.github.com>
2025-04-10 15:57:37 +0200

    Created and edited sass and component
    Created home and nav in sass, created Home component, edited colors partial in sass.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <h1>Home</h1>;
+import ProfileCard from "./ProfileCard"
+export default function Home({groupmembers}) {
+  return (
+    
+    <main>
+      
+      <h1>Gruppemedlemmer</h1>
+      
+        {
+
+          groupmembers.map((groupmember, index) => <ProfileCard key={index} member={groupmember}/>)
+
+        }
+
+        <section id="worklog">
+
+
+        </section>
+      
+    </main>
+
+
+  ); 
 }
