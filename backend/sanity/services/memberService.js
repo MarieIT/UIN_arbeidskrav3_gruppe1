b76d82b13f3bb15ed4569cac37b2820b756993c1
MaRoeIT <142741541+MaRoeIT@users.noreply.github.com>
2025-04-10 17:40:02 +0200

    Updated member service and fixed small bugs in the code

--- backend/sanity/services/memberService.js
+++ backend/sanity/services/memberService.js
@@ -2,11 +2,15 @@ import { client } from "../client";
 
 export async function fetchProfileCardInfo(){
     const data = await client.fetch(
-        `*[_type == 'member']{
+        `*[_type == 'member']|order(name asc){
             "image": frontpageimage.asset -> url,
             name,
             email
         }`
     )
     return data
+}
+
+export async function fetchProfilePageInfo() {
+    
 }
